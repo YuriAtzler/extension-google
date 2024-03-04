52007729e0a95ce2a2554cfaea275d5086a3fa62
YuriAtzler <atzleryuri@gmail.com>
2024-03-04 17:10:31 -0300

    feat:ajustes

--- contentScript.js
+++ contentScript.js
@@ -1,17 +1,61 @@
-chrome.storage.local.get(null, function (items) {
-  for (let key in items) {
-    let value = items[key];
+// Função para verificar e aplicar os parâmetros de consulta da URL
+function applyQueryParams() {
+  chrome.storage.local.get(null, function (items) {
     let url = new URL(window.location.href);
-    if (!url.search) {
-      url.search = `?${key}=${value}`;
-    } else {
-      url.searchParams.set(key, value);
+    let hasChanged = false;
+    let teste = [];
+    for (let key in items) {
+      let value = items[key];
+
+      teste.push({ label: key, value: value });
+
+      if (!url.searchParams.has(key)) {
+        url.searchParams.set(key, value);
+        hasChanged = true;
+      }
     }
-    window.history.replaceState(null, null, url.toString());
-  }
-});
+
+    if (hasChanged) {
+      history.replaceState(null, null, url.toString());
+      // Recarrega a página após aplicar os parâmetros de consulta
+      location.reload();
+    }
+
+    let container = document.getElementById("queryContainer");
+
+    teste.forEach((item) => {
+      let inputKey = document.createElement("input");
+      let inputValue = document.createElement("input");
+
+      inputKey.setAttribute("type", "text");
+      inputKey.setAttribute("id", item.key);
+      inputKey.setAttribute("value", item.key);
+
+      inputValue.setAttribute("type", "text");
+      inputValue.setAttribute("id", item.value);
+      inputValue.setAttribute("value", item.value);
+
+      let container2 = document.createElement("div");
+      container2.style.display = "flex";
+
+      container2.appendChild(inputKey);
+      container2.appendChild(inputValue);
+
+      container.appendChild(container2);
+    });
+  });
+}
+
+// Aplicar os parâmetros de consulta ao iniciar a extensão
+applyQueryParams();
+
+// Listener para atualizar os parâmetros de consulta quando houver mudanças
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   let url = new URL(window.location.href);
-  url.searchParams.set(message.key, message.value);
-  window.history.replaceState(null, null, url.toString());
+  if (!url.searchParams.has(message.key)) {
+    url.searchParams.set(message.key, message.value);
+    history.replaceState(null, null, url.toString());
+    // Recarrega a página após aplicar os parâmetros de consulta
+    location.reload();
+  }
 });
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.getElementById("saveButton").addEventListener("click", function () {
   var key = document.getElementById("key").value;
   var value = document.getElementById("value").value;
+
   chrome.storage.local.set({ [key]: value }, function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
       chrome.tabs.sendMessage(tabs[0].id, { key: key, value: value });
