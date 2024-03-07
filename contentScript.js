d8a42d9a3dc56861aa097af6d388d8205dc209a4
Yuri Nunes Atzler <yurinunesatzler@Yuris-MacBook-Air.local>
2024-03-06 23:13:54 -0300

    feat: ajustes no layout

--- contentScript.js
+++ contentScript.js
@@ -1,61 +1,61 @@
-// Função para verificar e aplicar os parâmetros de consulta da URL
-function applyQueryParams() {
-  chrome.storage.local.get(null, function (items) {
-    let url = new URL(window.location.href);
-    let hasChanged = false;
-    let teste = [];
-    for (let key in items) {
-      let value = items[key];
+// // Função para verificar e aplicar os parâmetros de consulta da URL
+// function applyQueryParams() {
+//   chrome.storage.local.get(null, function (items) {
+//     let url = new URL(window.location.href);
+//     let hasChanged = false;
+//     let teste = [];
+//     for (let key in items) {
+//       let value = items[key];
 
-      teste.push({ label: key, value: value });
+//       teste.push({ label: key, value: value });
 
-      if (!url.searchParams.has(key)) {
-        url.searchParams.set(key, value);
-        hasChanged = true;
-      }
-    }
+//       if (!url.searchParams.has(key)) {
+//         url.searchParams.set(key, value);
+//         hasChanged = true;
+//       }
+//     }
 
-    if (hasChanged) {
-      history.replaceState(null, null, url.toString());
-      // Recarrega a página após aplicar os parâmetros de consulta
-      location.reload();
-    }
+//     if (hasChanged) {
+//       history.replaceState(null, null, url.toString());
+//       // Recarrega a página após aplicar os parâmetros de consulta
+//       location.reload();
+//     }
 
-    let container = document.getElementById("queryContainer");
+//     let container = document.getElementById("queryContainer");
 
-    teste.forEach((item) => {
-      let inputKey = document.createElement("input");
-      let inputValue = document.createElement("input");
+//     teste.forEach((item) => {
+//       let inputKey = document.createElement("input");
+//       let inputValue = document.createElement("input");
 
-      inputKey.setAttribute("type", "text");
-      inputKey.setAttribute("id", item.key);
-      inputKey.setAttribute("value", item.key);
+//       inputKey.setAttribute("type", "text");
+//       inputKey.setAttribute("id", item.key);
+//       inputKey.setAttribute("value", item.key);
 
-      inputValue.setAttribute("type", "text");
-      inputValue.setAttribute("id", item.value);
-      inputValue.setAttribute("value", item.value);
+//       inputValue.setAttribute("type", "text");
+//       inputValue.setAttribute("id", item.value);
+//       inputValue.setAttribute("value", item.value);
 
-      let container2 = document.createElement("div");
-      container2.style.display = "flex";
+//       let container2 = document.createElement("div");
+//       container2.style.display = "flex";
 
-      container2.appendChild(inputKey);
-      container2.appendChild(inputValue);
+//       container2.appendChild(inputKey);
+//       container2.appendChild(inputValue);
 
-      container.appendChild(container2);
-    });
-  });
-}
+//       container.appendChild(container2);
+//     });
+//   });
+// }
 
-// Aplicar os parâmetros de consulta ao iniciar a extensão
-applyQueryParams();
+// // Aplicar os parâmetros de consulta ao iniciar a extensão
+// applyQueryParams();
 
-// Listener para atualizar os parâmetros de consulta quando houver mudanças
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  let url = new URL(window.location.href);
-  if (!url.searchParams.has(message.key)) {
-    url.searchParams.set(message.key, message.value);
-    history.replaceState(null, null, url.toString());
-    // Recarrega a página após aplicar os parâmetros de consulta
-    location.reload();
-  }
-});
+// // Listener para atualizar os parâmetros de consulta quando houver mudanças
+// chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+//   let url = new URL(window.location.href);
+//   if (!url.searchParams.has(message.key)) {
+//     url.searchParams.set(message.key, message.value);
+//     history.replaceState(null, null, url.toString());
+//     // Recarrega a página após aplicar os parâmetros de consulta
+//     location.reload();
+//   }
+// });
--- popup.js
+++ popup.js
@@ -1,10 +1,83 @@
-document.getElementById("saveButton").addEventListener("click", function () {
-  var key = document.getElementById("key").value;
-  var value = document.getElementById("value").value;
+document.getElementById("save-button").addEventListener("click", function () {
+  let domain = document.getElementById("domain");
+  let key = document.getElementById("key");
+  let value = document.getElementById("value");
 
-  chrome.storage.local.set({ [key]: value }, function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, { key: key, value: value });
-    });
+  let obj = {};
+  obj[domain.value] = { key: key.value, value: value.value };
+  chrome.storage.local.set(obj, function () { });
+
+  domain.value = "";
+  key.value = "";
+  value.value = "";
+
+  updatePopup();
+})
+
+function updatePopup() {
+  chrome.storage.local.get(null, function (items) {
+    let container = document.getElementById("container-items");
+    let message = document.getElementById("no-register-message");
+
+    if (Object.keys(items).length > 0) message.style.display = "none";
+    else message.style.display = "block";
+
+    while (container.firstChild) {
+      container.removeChild(container.firstChild);
+    }
+
+    for (let domain in items) {
+      let key = items[domain].key;
+      let value = items[domain].value;
+
+      let inputDomain = document.createElement("input");
+      let inputKey = document.createElement("input");
+      let inputValue = document.createElement("input");
+      let saveButton = document.createElement("button");
+      let removeButton = document.createElement("button");
+
+      saveButton.innerHTML = "Save";
+      removeButton.innerHTML = "Remove";
+
+      saveButton.addEventListener("click", function () {
+        let obj = {};
+        obj[domain] = { key: inputKey.value, value: inputValue.value };
+        chrome.storage.local.set(obj, function () { });
+      })
+
+      removeButton.addEventListener("click", function () {
+        chrome.storage.local.remove(domain, function () { });
+        updatePopup();
+      })
+
+      inputDomain.setAttribute("type", "text");
+      inputDomain.setAttribute("id", domain);
+      inputDomain.setAttribute("value", domain);
+      inputDomain.className = "field";
+
+      inputKey.setAttribute("type", "text");
+      inputKey.setAttribute("id", key);
+      inputKey.setAttribute("value", key);
+      inputKey.className = "field";
+
+      inputValue.setAttribute("type", "text");
+      inputValue.setAttribute("id", value);
+      inputValue.setAttribute("value", value);
+      inputValue.className = "field";
+
+      let container2 = document.createElement("div");
+      container2.className = "container-field";
+
+      container2.appendChild(inputDomain);
+      container2.appendChild(inputKey);
+      container2.appendChild(inputValue);
+      container2.appendChild(saveButton);
+      container2.appendChild(removeButton);
+
+      container.appendChild(container2);
+    }
   });
-});
+}
+
+updatePopup();
+
